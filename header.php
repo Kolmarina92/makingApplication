f215e9b4d23bdfb051ea21ba84aeca4fbcf45171
Kolmarina92 <kolmarina64@mail.ru>
2013-10-13 14:32:40 +0400

    footer and header has been added

---
+++ header.php
@@ -0,0 +1,5 @@
+<div id="templatemo_footer_wrapper">
+    <div id="templatemo_footer">
+        Все права защищены © 2013 <a href="#">Заявки на ПРЦ НИТ</a> | Разработано <a href="http://www.templatemo.com" target="_parent">Мариной Колесниковой</a>
+    </div> <!-- end of footer -->
+</div>
